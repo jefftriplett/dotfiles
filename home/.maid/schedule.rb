891837d16fb7c768fcf7ab761d54d816ca425d59
Jeff Triplett <jeff.triplett@gmail.com>
2014-06-01 16:58:24 -0500

    Updated cronjobs to be less chatty.

--- home/.maid/schedule.rb
+++ home/.maid/schedule.rb
@@ -1,9 +1,9 @@
 require 'whenever'
 
 every 30.minutes do
-  command "maid clean --silent"
+  command "maid clean --noop --silent"
 end
 
 every 24.hours do
-  command "maid clean --silent --rules=~/.maid/daily.rb"
+  command "maid clean --noop --silent --rules=~/.maid/daily.rb"
 end
