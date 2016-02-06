29550514c83467263bf7ce302302716ff4dbaf9f
Jeff Triplett <jeff.triplett@gmail.com>
2016-02-06 16:30:56 -0600

    Adds hammerspoon and configs

---
+++ home/.hammerspoon/init.lua
@@ -0,0 +1,163 @@
+
+----------------------
+-- Define hyper key --
+----------------------
+
+require 'keys'
+require 'tabletools'
+require 'sizeup'
+
+-------------------
+-- reload script --
+-------------------
+
+hs.hotkey.bind(hyper, "S", function()
+    hs.reload()
+end)
+
+
+----------
+-- Grid --
+----------
+
+hs.grid.GRIDWIDTH = 4
+hs.grid.GRIDHEIGHT = 3
+hs.grid.MARGINX = 0
+hs.grid.MARGINY = 0
+
+hs.hotkey.bind(hyper, 'g', hs.grid.show)
+-- hs.hotkey.bind(hyper, 'Left', hs.grid.pushWindowLeft)
+-- hs.hotkey.bind(hyper, 'Right', hs.grid.pushWindowRight)
+-- hs.hotkey.bind(hyper, 'Up', hs.grid.pushWindowUp)
+-- hs.hotkey.bind(hyper, 'Down', hs.grid.pushWindowDown)
+
+-- Monitor names
+local display_laptop = "Color LCD"
+local display_monitor = "Thunderbolt Display"
+
+-- Watchers and other useful objects
+local app_watcher = nil
+local battery_watcher = nil
+local caffeinate_watcher = nil
+local config_file_watcher = nil
+local screen_watcher = nil
+local usb_watcher = nil
+local wifi_watcher = nil
+
+----------------------
+-- System Callbacks --
+----------------------
+
+function app_changed_callback(app_name, event_type, app_object)
+    print('app_changed_callback()')
+    print(app_name, event_type, app_object)
+end
+
+function battery_changed_callback()
+    print('battery_changed_callback()')
+    print('timeRemaining:', hs.battery.timeRemaining())
+    print('percentage:', hs.battery.percentage())
+end
+
+function caffeinate_changed_callback(event_type)
+    print('caffeinate_changed_callback()')
+    print(event_type)
+    print(table.show(event_type, "event_type"))
+    if (event_type == hs.caffeinate.watcher.screensDidSleep) then
+        print('screensDidSleep')
+    elseif (event_type == hs.caffeinate.watcher.screensDidWake) then
+        print('screensDidWake')
+    end
+end
+
+function config_changed_callback(paths)
+    print('config_changed_callback()')
+    print(table.show(paths, "paths"))
+end
+
+function screens_changed_callback()
+    print('screens_changed_callback()')
+    number_of_screens = #hs.screen.allScreens()
+    print('number_of_screens:', number_of_screens)
+    print(table.show(hs.screen.allScreens(), "allScreens"))
+    for _, screen in pairs(hs.screen.allScreens()) do
+        print(table.show(screen:currentMode(), "currentMode"))
+    end
+end
+
+function wifi_changed_callback()
+    print('wifi_changed_callback()')
+    ssid = hs.wifi.currentNetwork()
+    print('ssid:', ssid)
+end
+
+function usb_changed_callback(data)
+    print('usb_changed_callback()')
+    print(table.show(data, "data"))
+end
+
+---------------------
+-- System Watchers --
+---------------------
+
+app_watcher = hs.application.watcher.new(app_changed_callback):start()
+battery_watcher = hs.battery.watcher.new(battery_changed_callback):start()
+caffeinate_watcher = hs.caffeinate.watcher.new(caffeinate_changed_callback):start()
+config_file_watcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", config_changed_callback):start()
+screen_watcher = hs.screen.watcher.new(screens_changed_callback):start()
+usb_watcher = hs.usb.watcher.new(usb_changed_callback):start()
+wifi_watcher = hs.wifi.watcher.new(wifi_changed_callback):start()
+
+
+-- Toggle an application between being the frontmost app, and being hidden
+function toggle_application(_app)
+    local app = hs.appfinder.appFromName(_app)
+    if not app then
+        -- FIXME: This should really launch _app
+        return
+    end
+    local mainwin = app:mainWindow()
+    if mainwin then
+        if mainwin == hs.window.focusedWindow() then
+            mainwin:application():hide()
+        else
+            mainwin:application():activate(true)
+            mainwin:application():unhide()
+            mainwin:focus()
+        end
+    end
+end
+
+-------------------------
+-- Application Hotkeys --
+-------------------------
+
+-- debug watcher statuses
+hs.hotkey.bind(hyper, '0', function()
+    print('app_watcher:', app_watcher)
+    print('battery_watcher:', battery_watcher)
+    print('caffeinate_watcher:', caffeinate_watcher)
+    print('config_file_watcher:', config_file_watcher)
+    print('screen_watcher:', screen_watcher)
+    print('usb_watcher:', usb_watcher)
+    print('wifi_watcher:', wifi_watcher)
+end)
+
+hs.hotkey.bind(hyper, 'Q', function()
+    toggle_application("iTerm")
+end)
+
+hs.hotkey.bind(hyper, 'W', function()
+    toggle_application("Sublime Text")
+end)
+
+hs.hotkey.bind(hyper, '.', function()
+    hs.hints.windowHints(hs.window.focusedWindow():application():allWindows())
+end)
+
+hs.hotkey.bind(hyper, ',', function()
+    battery_changed_callback()
+    screens_changed_callback()
+end)
+
+hs.alert.show("Config loaded")
---
+++ home/.hammerspoon/sizeup.lua
@@ -0,0 +1,334 @@
+-- === sizeup ===
+--
+-- SizeUp emulation for hammerspoon
+--
+-- To use, you can tweak the key bindings and the margins
+
+require 'keys'
+
+local sizeup = { }
+-- local hyper = {"ctrl", "alt", "cmd"}
+
+--------------
+-- Bindings --
+--------------
+
+--- Split Screen Actions ---
+-- Send Window Left
+hs.hotkey.bind(hyper, "Left", function()
+  sizeup.send_window_left()
+end)
+-- Send Window Right
+hs.hotkey.bind(hyper, "Right", function()
+  sizeup.send_window_right()
+end)
+-- Send Window Up
+hs.hotkey.bind(hyper, "Up", function()
+  sizeup.send_window_up()
+end)
+-- Send Window Down
+hs.hotkey.bind(hyper, "Down", function()
+  sizeup.send_window_down()
+end)
+
+--- Quarter Screen Actions ---
+-- Send Window Upper Left
+hs.hotkey.bind({"ctrl", "alt", "shift"}, "Left", function()
+  sizeup.send_window_upper_left()
+end)
+-- Send Window Upper Right
+hs.hotkey.bind({"ctrl","alt", "shift"}, "Up", function()
+  sizeup.send_window_upper_right()
+end)
+-- Send Window Lower Left
+hs.hotkey.bind({"ctrl", "alt", "shift"}, "Down", function()
+  sizeup.send_window_lower_left()
+end)
+-- Send Window Lower Right
+hs.hotkey.bind({"ctrl", "alt", "shift"}, "Right", function()
+  sizeup.send_window_lower_right()
+end)
+
+--- Multiple Monitor Actions ---
+-- Send Window Prev Monitor
+hs.hotkey.bind({ "ctrl", "alt" }, "Left", function()
+  sizeup.send_window_prev_monitor()
+end)
+-- Send Window Next Monitor
+hs.hotkey.bind({ "ctrl", "alt" }, "Right", function()
+  sizeup.send_window_next_monitor()
+end)
+
+--- Spaces Actions ---
+
+-- Apple no longer provides any reliable API access to spaces.
+-- As such, this feature no longer works in SizeUp on Yosemite and
+-- Hammerspoon currently has no solution that isn't a complete hack.
+-- If you have any ideas, please visit the ticket
+
+--- Snapback Action ---
+hs.hotkey.bind(hyper, "Z", function()
+  sizeup.snapback()
+end)
+
+--- Other Actions ---
+
+-- Make Window Full Screen
+hs.hotkey.bind(hyper, "M", function()
+  sizeup.maximize()
+end)
+
+-- Send Window Center
+hs.hotkey.bind(hyper, "C", function()
+  sizeup.move_to_center_relative({w=0.60, h=0.60})
+end)
+
+
+-------------------
+-- Configuration --
+-------------------
+
+-- Margins --
+sizeup.screen_edge_margins = {
+  top = 0, -- px
+  left = 0,
+  right = 0,
+  bottom = 0
+}
+sizeup.partition_margins = {
+  x = 0, -- px
+  y = 0
+}
+
+-- Partitions --
+sizeup.split_screen_partitions = {
+  x = 0.5, -- %
+  y = 0.5
+}
+sizeup.quarter_screen_partitions = {
+  x = 0.5, -- %
+  y = 0.5
+}
+
+
+----------------
+-- Public API --
+----------------
+
+function sizeup.send_window_left()
+  local s = sizeup.screen()
+  local ssp = sizeup.split_screen_partitions
+  local g = sizeup.gutter()
+  sizeup.set_frame("Left", {
+    x = s.x,
+    y = s.y,
+    w = (s.w * ssp.x) - sizeup.gutter().x,
+    h = s.h
+  })
+end
+
+function sizeup.send_window_right()
+  local s = sizeup.screen()
+  local ssp = sizeup.split_screen_partitions
+  local g = sizeup.gutter()
+  sizeup.set_frame("Right", {
+    x = s.x + (s.w * ssp.x) + g.x,
+    y = s.y,
+    w = (s.w * (1 - ssp.x)) - g.x,
+    h = s.h
+  })
+end
+
+function sizeup.send_window_up()
+  local s = sizeup.screen()
+  local ssp = sizeup.split_screen_partitions
+  local g = sizeup.gutter()
+  sizeup.set_frame("Up", {
+    x = s.x,
+    y = s.y,
+    w = s.w,
+    h = (s.h * ssp.y) - g.y
+  })
+end
+
+function sizeup.send_window_down()
+  local s = sizeup.screen()
+  local ssp = sizeup.split_screen_partitions
+  local g = sizeup.gutter()
+  sizeup.set_frame("Down", {
+    x = s.x,
+    y = s.y + (s.h * ssp.y) + g.y,
+    w = s.w,
+    h = (s.h * (1 - ssp.y)) - g.y
+  })
+end
+
+function sizeup.send_window_upper_left()
+  local s = sizeup.screen()
+  local qsp = sizeup.quarter_screen_partitions
+  local g = sizeup.gutter()
+  sizeup.set_frame("Upper Left", {
+    x = s.x,
+    y = s.y,
+    w = (s.w * qsp.x) - g.x,
+    h = (s.h * qsp.y) - g.y
+  })
+end
+
+function sizeup.send_window_upper_right()
+  local s = sizeup.screen()
+  local qsp = sizeup.quarter_screen_partitions
+  local g = sizeup.gutter()
+  sizeup.set_frame("Upper Right", {
+    x = s.x + (s.w * qsp.x) + g.x,
+    y = s.y,
+    w = (s.w * (1 - qsp.x)) - g.x,
+    h = (s.h * (qsp.y)) - g.y
+  })
+end
+
+function sizeup.send_window_lower_left()
+  local s = sizeup.screen()
+  local qsp = sizeup.quarter_screen_partitions
+  local g = sizeup.gutter()
+  sizeup.set_frame("Lower Left", {
+    x = s.x,
+    y = s.y + (s.h * qsp.y) + g.y,
+    w = (s.w * qsp.x) - g.x,
+    h = (s.h * (1 - qsp.y)) - g.y
+  })
+end
+
+function sizeup.send_window_lower_right()
+  local s = sizeup.screen()
+  local qsp = sizeup.quarter_screen_partitions
+  local g = sizeup.gutter()
+  sizeup.set_frame("Lower Right", {
+    x = s.x + (s.w * qsp.x) + g.x,
+    y = s.y + (s.h * qsp.y) + g.y,
+    w = (s.w * (1 - qsp.x)) - g.x,
+    h = (s.h * (1 - qsp.y)) - g.y
+  })
+end
+
+function sizeup.send_window_prev_monitor()
+  hs.alert.show("Prev Monitor")
+  local win = hs.window.focusedWindow()
+  local nextScreen = win:screen():previous()
+  win:moveToScreen(nextScreen)
+end
+
+function sizeup.send_window_next_monitor()
+  hs.alert.show("Next Monitor")
+  local win = hs.window.focusedWindow()
+  local nextScreen = win:screen():next()
+  win:moveToScreen(nextScreen)
+end
+
+-- snapback return the window to its last position. calling snapback twice returns the window to its original position.
+-- snapback holds state for each window, and will remember previous state even when focus is changed to another window.
+function sizeup.snapback()
+  local win = sizeup.win()
+  local id = win:id()
+  local state = win:frame()
+  local prev_state = sizeup.snapback_window_state[id]
+  if prev_state then
+    win:setFrame(prev_state)
+  end
+  sizeup.snapback_window_state[id] = state
+end
+
+function sizeup.maximize()
+  sizeup.set_frame("Full Screen", sizeup.screen())
+end
+
+--- move_to_center_relative(size)
+--- Method
+--- Centers and resizes the window to the the fit on the given portion of the screen.
+--- The argument is a size with each key being between 0.0 and 1.0.
+--- Example: win:move_to_center_relative(w=0.5, h=0.5) -- window is now centered and is half the width and half the height of screen
+function sizeup.move_to_center_relative(unit)
+  local s = sizeup.screen()
+  sizeup.set_frame("Center", {
+    x = s.x + (s.w * ((1 - unit.w) / 2)),
+    y = s.y + (s.h * ((1 - unit.h) / 2)),
+    w = s.w * unit.w,
+    h = s.h * unit.h
+  })
+end
+
+--- move_to_center_absolute(size)
+--- Method
+--- Centers and resizes the window to the the fit on the given portion of the screen given in pixels.
+--- Example: win:move_to_center_relative(w=800, h=600) -- window is now centered and is 800px wide and 600px high
+function sizeup.move_to_center_absolute(unit)
+  local s = sizeup.screen()
+  sizeup.set_frame("Center", {
+    x = (s.w - unit.w) / 2,
+    y = (s.h - unit.h) / 2,
+    w = unit.w,
+    h = unit.h
+  })
+end
+
+
+------------------
+-- Internal API --
+------------------
+
+-- SizeUp uses no animations
+hs.window.animation_duration = 0.0
+
+-- Initialize Snapback state
+sizeup.snapback_window_state = { }
+
+-- return currently focused window
+function sizeup.win()
+  return hs.window.focusedWindow()
+end
+
+-- display title, save state and move win to unit
+function sizeup.set_frame(title, unit)
+  hs.alert.show(title)
+  local win = sizeup.win()
+  sizeup.snapback_window_state[win:id()] = win:frame()
+  return win:setFrame(unit)
+end
+
+-- screen is the available rect inside the screen edge margins
+function sizeup.screen()
+  local screen = sizeup.win():screen():frame()
+  local sem = sizeup.screen_edge_margins
+  return {
+    x = screen.x + sem.left,
+    y = screen.y + sem.top,
+    w = screen.w - (sem.left + sem.right),
+    h = screen.h - (sem.top + sem.bottom)
+  }
+end
+
+-- gutter is the adjustment required to accomidate partition
+-- margins between windows
+function sizeup.gutter()
+  local pm = sizeup.partition_margins
+  return {
+    x = pm.x / 2,
+    y = pm.y / 2
+  }
+end
+
+--- hs.window:moveToScreen(screen)
+--- Method
+--- move window to the the given screen, keeping the relative proportion and position window to the original screen.
+--- Example: win:moveToScreen(win:screen():next()) -- move window to next screen
+function hs.window:moveToScreen(nextScreen)
+  local currentFrame = self:frame()
+  local screenFrame = self:screen():frame()
+  local nextScreenFrame = nextScreen:frame()
+  self:setFrame({
+    x = ((((currentFrame.x - screenFrame.x) / screenFrame.w) * nextScreenFrame.w) + nextScreenFrame.x),
+    y = ((((currentFrame.y - screenFrame.y) / screenFrame.h) * nextScreenFrame.h) + nextScreenFrame.y),
+    h = ((currentFrame.h / screenFrame.h) * nextScreenFrame.h),
+    w = ((currentFrame.w / screenFrame.w) * nextScreenFrame.w)
+  })
+end
